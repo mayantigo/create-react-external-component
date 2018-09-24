75955a70273f75cc37c0fd7675214cd0f2c9f294
francisco.gonzalez <francisco.gonzalez@barceloviajes.com>
2018-09-24 14:00:08 +0200

    Add simple storybook.

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,19 +1,5 @@
 import React from 'react';
-
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import Component from '../src/components/component';
 
-import { Button, Welcome } from '@storybook/react/demo';
-
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
+storiesOf('Component', module).add('Simple storybook', () => <Component title="Simple component" />);
